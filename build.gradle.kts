aa36b83c354ab5ed7058c0633fe0a6cc7ad4eda3
vimanthan <159103666+vimanthan3@users.noreply.github.com>
2024-06-17 12:02:44 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,22 @@
 plugins {
-    id("java")
-}
-jar {
-    manifest {
-        attributes 'Main-Class': 'com.package.app.Class'
-    }
+    id 'java'
+    id 'org.cyclonedx.bom' version '1.8.1'
 }
 
 repositories {
     mavenCentral()
+}
+
+cyclonedxBom {
+    includeConfigs = ["runtimeClasspath"]
+    projectType = "application"
+    schemaVersion = "1.5"
+    destination = file("build/reports")
+    outputName = "bom"
+    outputFormat = "xml"
+    includeBomSerialNumber = false
+    includeLicenseText = false
+    componentVersion = "2.0.0"
 }
 
 dependencies {
@@ -26,34 +34,14 @@ java {
     }
 }
 
-tasks.jar {
+jar {
     manifest {
         attributes(
-            "Main-Class" to "fully.qualified.MainClass"
+            'Main-Class': 'com.package.app.Class'
         )
     }
 }
 
-
-
-
-
-repositories {
-    mavenCentral()
+tasks.withType(JavaCompile) {
+    options.encoding = 'UTF-8'
 }
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.2"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(8))
-    }
-}
-
